c48038f1d8939eff9471781d30a474b865c1c6d3
Henrique Siqueira Lima <henrynyyls@gmail.com>
2025-02-22 19:04:14 -0300

    Alot of paths were created and there's a new image systems
    Now the images that are sent to the backend are stored inside a folder named "uploads", that's where they are for when the user needs to see them later.
    
    Now you can create publications and send them to the database.

--- server.js
+++ server.js
@@ -2,8 +2,52 @@ const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
 const path = require("path");
+const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
+
+const uri =
+  "mongodb+srv://myAtlasDBUser:135790@myatlasclusteredu.ufhaxua.mongodb.net/ByteLandDatabase?retryWrites=true&w=majority&appName=myAtlasClusterEDU";
+
+//Esquema do usuário
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  image: {
+    type: String,
+    default: "https://cdn-icons-png.flaticon.com/512/711/711769.png",
+  },
+  createdAt: { type: Date, default: Date.now },
+});
+
+//Modelo do usuário
+const User = mongoose.model("User", userSchema);
+
+//Esquema da publicação
+const publicationSchema = new mongoose.Schema({
+  owner: { type: String, required: true },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String },
+  createdAt: { type: Date, default: Date.now },
+});
+
+//Modelo da pulicação
+const Publication = mongoose.model("Publication", publicationSchema);
+
+//Configuração Mongoose
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Conexão com o banco de dados efetuada com sucesso!");
+  })
+  .catch((err) => {
+    console.error("Erro na conexão com o banco de dados:", err);
+  });
 
 // Configuração do CORS
 app.use(
@@ -26,6 +70,10 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
+// Configura o diretório de uploads para servir arquivos estáticos
+const uploadsPath = path.join(__dirname, "uploads");
+app.use("/uploads", express.static(uploadsPath));
+
 // Middleware para permitir o uso de JSON no corpo das requisições
 app.use(express.json());
 
@@ -35,12 +83,21 @@ app.get("/", (req, res) => {
 });
 
 // Rota para receber dados via POST
-app.post("/publications", upload.single("image"), (req, res) => {
+app.post("/publications", upload.single("image"), async (req, res) => {
   try {
     const { owner, title, description } = req.body;
     const image = req.file ? req.file.path : null; // Caminho do arquivo de imagem
 
-    console.log("Dados recebidos:", { titulo, description, image });
+    const newPublication = new Publication({
+      owner: owner,
+      title: title,
+      description: description,
+      image: image,
+    });
+
+    await newPublication.save();
+
+    console.log("Dados recebidos:", { title, description, image });
     res.status(200).json({ message: "Publicação recebida com sucesso!" });
   } catch (error) {
     console.error("Erro ao processar a publicação:", error);
@@ -48,16 +105,60 @@ app.post("/publications", upload.single("image"), (req, res) => {
   }
 });
 
-app.post("/login", (req, res) => {
-  console.log("Requisição de login recebida!");
-  console.log("Dados recebidos:", req.body);
-  res.status(200).json({ message: "Login realizado com sucesso!" });
+app.get("/getPublications", async (req, res) => {
+  try {
+    const publications = await Publication.find();
+    res.json(publications);
+  } catch (error) {
+    console.error("Erro ao buscar publicações:", error);
+    res.status(500).json({ message: "Erro interno do servidor" });
+  }
 });
 
-app.post("/signup", (req, res) => {
-  console.log("Requisição de cadastro recebida!");
-  console.log("Dados recebidos:", req.body);
-  res.status(200).json({ message: "Cadast\ro realizado com sucesso!" });
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email: email, password: password });
+
+    if (!user) {
+      return res.status(404).json({ message: "Esse usuário não existe." });
+    } else {
+    }
+
+    res
+      .status(200)
+      .json({ message: "Login realizado com sucesso!", user: user });
+  } catch (error) {
+    console.error("Erro ao processar o login:", error);
+    res.status(500).json({ message: "Erro interno do servidor" });
+  }
+});
+
+app.post("/signup", async (req, res) => {
+  //Criação do perfil no banco de dados
+  try {
+    const { user, email, password } = req.body;
+    const newUser = new User({
+      name: user,
+      email: email,
+      password: password,
+    });
+
+    await newUser.save();
+    res
+      .status(200)
+      .json({ message: "Usuário criado com sucesso!", user: newUser });
+  } catch (error) {
+    if (error.code == 11000) {
+      res.status(409).json({
+        message:
+          "Alguma informação enviada já existe em outro usuário já existe!",
+      });
+    } else {
+      res.status(500).json({ message: "Erro interno do servidor:", error });
+    }
+  }
 });
 
 // Inicia o servidor
